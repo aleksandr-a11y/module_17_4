bc3cc448d3d7b46e55827343a0dcb6fc9af005be
aleksandr-a11y <117071816+aleksandr-a11y@users.noreply.github.com>
2024-12-19 14:14:03 +0300

    Add files via upload

---
+++ app/models/task.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from app.backend.db import Base
+from app.models import *
+
+class Task(Base):
+    __tablename__ = "tasks"
+    __table_args__ = {'keep_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
+    slug = Column(String, unique=True, index=True)
+
+    user = relationship('User', back_populates="tasks")
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Task.__table__))
+
+router = APIRouter(prefix='/task',tags=['task'])
+
+@router.get('/')
+async def aii_tasks():
+    pass
+
+@router.get('/task_id')
+async def task_by_id():
+    pass
+
+@router.post('/create')
+async def create_task():
+    pass
+
+@router.put('/update')
+async def update_task():
+    pass
+
+@router.delete('/delete')
+async def delete_task():
+    pass
